4e0fa5b9eb07115a238f7293f35c59ae63c8dd95
LMF709268224 <709268224@qq.com>
2026-03-25 17:49:01 +0800

    Clear cache

--- cmd/titan-edge/main.go
+++ cmd/titan-edge/main.go
@@ -217,8 +217,13 @@ func getSchedulerVersion(api api.Scheduler, timeout time.Duration) (api.APIVersi
 	return api.Version(ctx)
 }
 
-func newAuthTokenFromScheduler(schedulerURL, nodeID string, privateKey *rsa.PrivateKey) (string, error) {
-	schedulerAPI, closer, err := client.NewScheduler(context.Background(), schedulerURL, nil, jsonrpc.WithHTTPClient(client.NewHTTP3Client()))
+func newAuthTokenFromScheduler(transport *quic.Transport, schedulerURL, nodeID string, privateKey *rsa.PrivateKey) (string, error) {
+	httpClient, err := client.NewHTTP3ClientWithPacketConn(transport)
+	if err != nil {
+		return "", err
+	}
+
+	schedulerAPI, closer, err := client.NewScheduler(context.Background(), schedulerURL, nil, jsonrpc.WithHTTPClient(httpClient))
 	if err != nil {
 		return "", err
 	}
@@ -254,7 +259,7 @@ func getAccessPoint(locatorURL, nodeID, areaID string) (*types.AccessPointRsp, e
 }
 
 func newSchedulerAPI(transport *quic.Transport, schedulerURL, nodeID string, privateKey *rsa.PrivateKey) (api.Scheduler, jsonrpc.ClientCloser, error) {
-	token, err := newAuthTokenFromScheduler(schedulerURL, nodeID, privateKey)
+	token, err := newAuthTokenFromScheduler(transport, schedulerURL, nodeID, privateKey)
 	if err != nil {
 		return nil, nil, err
 	}
